14e57a7e19ec817e693d85494bedaaa9e96a8410
kakaomame <nattyannkunndesu@icloud.com>
2026-01-02 17:52:18 +0900

    Create sw.js

---
+++ data/rpg/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'gemini-sonic-v1';
+const ASSETS = [
+  './',
+  './audiov15.html',
+  './sw.js',
+  './lang.json',
+  './ja_jp.json',
+  './en_eu.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js',
+  '/rei/logo.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
